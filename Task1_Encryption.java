e0e8884f8e37e81fab464ff6a609a5ead18b6f0c
Shada <shada03a@gmail.com>
2025-11-03 21:14:53 +0300

    Rename class to Task1_Encryption

--- Task1_Encryption.java
+++ Task1_Encryption.java
@@ -1,7 +1,7 @@
 
 
 import java.util.Scanner;
-public class DoubleTransposition{
+public class Task1_Encryption{
     public static void main(String[]args){
 
         Scanner scanner = new Scanner(System.in);
